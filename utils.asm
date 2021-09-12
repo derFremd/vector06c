414e22396ec7c713824df59f326547c51c479ee9
Sergey Semenikhin <der.fremd@gmail.com>
2021-09-13 01:52:33 +0300

    added repeat code of char

--- utils.asm
+++ utils.asm
@@ -1,11 +1,11 @@
-	; ********************************************	
-	; Various subroutines-utilities in the assembly 
+	; ********************************************
+	; Various subroutines-utilities in the assembly
 	; language for the Vector 06c personal computer.
 	; @author Sergey S. (der.fremd@gmail.com)
  	; @version 1.0
 	; ********************************************
 
-	; ***************************	
+	; ***************************
 	; Процедура clear_scr - очистка экрана
 	; Используется маска запрещенных плоскостей по адресу (scr_mask)
 	; ***************************
@@ -13,24 +13,24 @@ clear_scr:
 	push hl
 	ld l,0
 	ld a,(scr_mask)
-
+	; -----------------------
 	ld h,$00
-	rrca	
+	rrca
 	call c,clear_scr_plane
 	ld h,$e0
-	rrca	
-	call c,clear_scr_plane	
+	rrca
+	call c,clear_scr_plane
 	ld h,$c0
-	rrca	
-	call c,clear_scr_plane	
+	rrca
+	call c,clear_scr_plane
 	ld h,$a0
 	rrca
-	call c,clear_scr_plane	
-
+	call c,clear_scr_plane
+	; -----------------------
 	pop hl
 	ret
 
-	; ***************************	
+	; ***************************
 	; Очистка одной плоскости экрана 8K.
 	; hl - адрес начала
 	; ***************************
@@ -41,7 +41,7 @@ clear_scr_plane:
 	push de
 	push bc
 	ex de,hl				; временно hl -> de
-	ld hl,0					; сохраняем значение sp в de 
+	ld hl,0					; сохраняем значение sp в de
 	add hl,sp
 	ex de,hl				; возвращаем hl <- de
 	ld sp,hl				; в sp адрес начало очистки
@@ -63,9 +63,9 @@ ENDM
 	ret
 
 	; ***************************
-	; Процедура set_color - устанавливает цвет и фона символа 
+	; Процедура set_color - устанавливает цвет и фона символа
 	; входные параметры: A = (цвет_символа + 16 * цвет_фона)
-	; ***************************	
+	; ***************************
 set_color:
 	ld (char_color),a
 	ret
@@ -83,7 +83,7 @@ set_color_char:
 	or c
 	ld (char_color),a
 	pop bc
-	ret	
+	ret
 
 	; ***************************
 	; Процедура set_color_bg - устанавливает только цвет фона символов
@@ -93,7 +93,7 @@ set_color_bg:
 	push bc
 	and $0f
 	rlca
-	rlca	
+	rlca
 	rlca
 	rlca
 	ld c,a
@@ -102,11 +102,11 @@ set_color_bg:
 	or c
 	ld (char_color),a
 	pop bc
-	ret		
+	ret
 
 get_color:
 	ld a,(char_color)
-	ret	
+	ret
 
 	; ***************************
 	; Процедура set_cur - устанавливает курсор символов
@@ -118,40 +118,39 @@ set_cur:
 	ld h,a
 	ld a,l							; корректируем макс. значения y (0-31)
 	and $1f
-	ld l,a	
+	ld l,a
 	ld (cur_char_pos),hl			; сохранить текущую координату
 	ret
 
 	; ***************************
-	; Процедура print_str - печать строки на экране. конец строки $0 
-	; входные параметры: de - адрес начала строки 
-	; выходные параметры: de - адрес конца строки строки 
+	; Процедура print_str - печать строки на экране. конец строки $0
+	; входные параметры: de - адрес начала строки
+	; выходные параметры: de - адрес конца строки строки
 	; Служебные символы:
-	; $10 - изменить цвет (2 байта),
-	; $11 - изменить фон (2 байта)
-	; $16 - новое положение (3 байта)
+	; $10 - изменить цвет (2 байта: $10,$zz),
+	; $11 - изменить фон (2 байта: $11,$zz)
+	; $16 - новое положение (3 байта: $16,$xx,$yy)
 	; $08 - забой (1 байт)
 	; $0A - перевод строки (1 байт)
 	; $0D - возврат коретки (1 байт)
-	; $09 - табуляция (1 байт), см. tab_length:
-	; TODO 
-	; 
-	; ***************************	
+	; $09 - табуляция (1 байт), см. tab_length и tab_placeholder:
+	; $02 - повтор следующего после команды символа от 0-255 раз (2 байта: $02,$nn,Char)
+	; ***************************
 print_str:
 	push hl
 	push bc
 	; -----------------------
 	ld hl,(cur_char_pos) 			; загрузить текущую координату
-_print_str_cur_to_scr_addr:	
+_print_str_cur_to_scr_addr:
 	call char_coord_to_scr_addr		; конвертировать в адрес на экране
 	; -----------------------
-_print_str_repeat:	
+_print_str_repeat:
 	ld a,(de)						; текущий символ
 	inc de							; указатель на след. символ
 	or a							; конец строки?
 	jp nz,_print_str_no_term		; нет, это не конец строки
 	call scr_addr_to_char_coord		; адрес экрана в координату
-	ld (cur_char_pos),hl			; сохранить 
+	ld (cur_char_pos),hl			; сохранить
 	pop bc
 	pop hl
 	ret								; выход
@@ -163,13 +162,13 @@ _print_str_one_char:
 	call print_char					; печать обычного символа
 	call print_str_move_cur_right
 	jp _print_str_repeat
-	; -----------------------	
+	; -----------------------
 	; проверяем возможные служебные символы
 _print_str_spec_char:
-	cp $10						
-	jp z,_print_str_new_fg			
-	cp $11						
-	jp z,_print_str_new_bg		
+	cp $10
+	jp z,_print_str_new_fg
+	cp $11
+	jp z,_print_str_new_bg
 	cp $16
 	jp z,_print_str_at_cur
 	cp $08
@@ -180,26 +179,28 @@ _print_str_spec_char:
 	jp z,_print_str_cr
 	cp $09
 	jp z,_print_str_tab
-	ld a,63						; нет такого кода, печатаем символ ?
+	cp $02
+	jp z,_print_str_rep_char
+	ld a,'?'					; нет такого кода, печатаем символ ?
 	jp _print_str_one_char
-	; -----------------------	
+	; -----------------------
 	; установка цвет символа (код $10)
 _print_str_new_fg:
-	ld a,(de)						
+	ld a,(de)
 	inc de
 	call set_color_char
 	jp _print_str_repeat
-	; -----------------------	
+	; -----------------------
 	; установка цвет фона символа (код $11)
 _print_str_new_bg:
-	ld a,(de)						
+	ld a,(de)
 	inc de
 	call set_color_bg
 	jp _print_str_repeat
 	; -----------------------
-	; новая координата (CUR x,y)
+	; новая координата (AT x,y)
 _print_str_at_cur:
-	ex de,hl						
+	ex de,hl
 	ld d,(hl)
 	inc hl
 	ld e,(hl)
@@ -208,16 +209,16 @@ _print_str_at_cur:
 	jp _print_str_cur_to_scr_addr
 	; -----------------------
 	; символ backspace (BS)
-_print_str_backspace:	
+_print_str_backspace:
 	dec h
 	ld a,$df
 	cp h
 	jp nz,_print_str_bs_char
 	ld h,$ff
-	ld a,l							; поднимаемся выше на 8 пикселей	
-	add a,$08							
-	ld l,a	
-_print_str_bs_char:	
+	ld a,l							; поднимаемся выше на 8 пикселей
+	add a,$08
+	ld l,a
+_print_str_bs_char:
 	ld a,$1f						; напечатать символ $1f
 	call print_char
 	jp _print_str_repeat
@@ -225,14 +226,14 @@ _print_str_bs_char:
 	; перевод строки (LF)
 _print_str_lf:
 	ld h,$e0
-	ld a,l							; спускаемся ниже на 8 пикселей	
-	sub $08							
+	ld a,l							; спускаемся ниже на 8 пикселей
+	sub $08
 	ld l,a
 	jp _print_str_repeat
 	; -----------------------
 	; возврат корретки (LF)
-_print_str_cr:	
-	ld h,$e0	
+_print_str_cr:
+	ld h,$e0
 	jp _print_str_repeat
 	; -----------------------
 	; символ табуляции (TAB)
@@ -241,14 +242,14 @@ _print_str_tab:
 	ld a,h
 	and $1f
 	ld b,a							; текущая координата по X
-	ld a,(tab_width)				
+	ld a,(tab_width)
 	ld c,a							; шаг табуляции
 	xor	a
 _print_str_tab_next_tab:
 	add a,c							; прибавлять пока не станет больше
 	cp b							; чем текущая координата по x
 	jp c,_print_str_tab_next_tab
-	;jp z,_print_str_tab_next_tab 
+	;jp z,_print_str_tab_next_tab
 	cp $20							; проверка на максимальное значение
 	jp c,_print_str_tab_no_max_val
 	ld a,$20
@@ -261,9 +262,27 @@ _print_str_tab_cycle:
 	dec b
 	jp m,_print_str_repeat
 	ld a,c
-	call print_char		
+	call print_char
 	call print_str_move_cur_right
 	jp _print_str_tab_cycle
+	; -----------------------
+	; повтор n раз символа после управляющего символа (REP)
+_print_str_rep_char:
+	ld a,(de)						; количество повторов
+	ld b,a
+	inc de
+	ld a,(de)						; повторяемый символ
+	cp ' '							; проверка на попытку повтора
+	jp c,_print_str_repeat			; символов с кодом меньше 0x20
+	ld c,a
+	inc de
+_print_str_rep_char_cycle:
+	dec b
+	jp m,_print_str_repeat
+	ld a,c
+	call print_char
+	call print_str_move_cur_right
+	jp _print_str_rep_char_cycle
 
 	; ***************************
 	; Процедура print_str_move_cur_right вычисляет адрес
@@ -273,9 +292,9 @@ print_str_move_cur_right:
 	inc h							; курсор вправо
 	ret nz							; выход, если не вышли за границу
 	ld h,$e0						; вышли за границу, восстановить H
-	ld a,l							; спускаемся ниже на 8 пикселей	
-	sub $08							
-	ld l,a	
+	ld a,l							; спускаемся ниже на 8 пикселей
+	sub $08
+	ld l,a
 	ret
 
 	; ***************************
@@ -298,7 +317,7 @@ char_coord_to_scr_addr:
 	ret
 
 	; ***************************
-	; Процедура scr_addr_to_char_coord переводит адрес на экране 
+	; Процедура scr_addr_to_char_coord переводит адрес на экране
 	; в координаты символа.
 	; вход: HL - адрес на экране
 	; выход: H - горизонтальная и L - вертикальная координата
@@ -311,7 +330,7 @@ scr_addr_to_char_coord:
 	rrca
 	rrca
 	rrca
-	and $1f	
+	and $1f
 	ld l,a
 	ret
 
@@ -320,9 +339,9 @@ scr_addr_to_char_coord:
 	; ***************************
 set_tab_width:
 	cp 32
-	jp c,set_tab_width_ok	
+	jp c,set_tab_width_ok
 	ld a,31
-set_tab_width_ok:	
+set_tab_width_ok:
 	ld (tab_width),a
 	ret
 
@@ -333,7 +352,7 @@ set_tab_placeholder:
 	cp $20
 	jp nc,set_tab_placeholder_ok
 	ld a,' '
-set_tab_placeholder_ok:	
+set_tab_placeholder_ok:
 	ld (tab_placeholder),a
 	ret
 
@@ -344,30 +363,6 @@ set_planes:
 	and $0f
 	ld (scr_mask),a
 	ret
-
-cur_char_pos:
-	DB 0,0			; хранится текущее положение курсора y,x (0-31)
-
-tab_width:
-	DB 4			; длина табуляции (0-31)
-
-tab_placeholder:
-	DEFM " "		; по умолчанию пробел
-
-char_color:			; текущий цвет символов и фона (символ + 16 * фон)
-	DB %01010011
-
-saved_sp:			; временное хранение значения регистра SP
-	DW $0000				
-
-cur_char_addr:		; сохраняем адрес текущего символа в таблице шрифта
-	DW $0000
-
-scr_addr_char:		; адрес на экране для вывода символа
-	DW $0000 
-
-scr_mask:			; маска запрета экранных плоскостей
-	DW %00001111
 
 	; *****************************
 	; Процедура print_char - вывод символа 8 байт (8x8 точек) в заданную часть экрана
@@ -391,7 +386,7 @@ print_char:
 	ld h,$00
 	add hl,hl				; умножить на 8
 	add hl,hl
-	add hl,hl	
+	add hl,hl
 	ld bc,font_table
 	add hl,bc
 	ld sp,hl				; в SP адрес символа в таблице шрифта
@@ -400,7 +395,7 @@ print_char:
 	ld c, %00010001			; маска выбора плоскости (биты 7-4 фон, биты 3-0 цвет символа)
 	; ---------------------------
 _print_char_repeat:
-	ld a,(scr_mask)					; маска запрета плоскостей	
+	ld a,(scr_mask)					; маска запрета плоскостей
 	and c
 	jp z,_print_char_next_plane		; ничего не делать
 	; ---------------------------
@@ -410,7 +405,7 @@ _print_char_repeat:
 	jp nz,_print_char_is_bg			; что-то из этого есть, переход
 	; ---------------------------
 	; нет ни цвета, ни фона. заполняем 8 нулями
-	xor a					
+	xor a
 REPT 7
 	ld (hl),a
 	inc l
@@ -421,10 +416,10 @@ ENDM
 _print_char_is_bg:
 	and $f0							; если ли фон?
 	jp nz,_print_char_is_color		; переход если есть фон
-	; ---------------------------		
+	; ---------------------------
 	; фона нет, заполняем изображением символа из таблицы (8 байт)
 REPT 3
-	pop de					
+	pop de
 	ld (hl),e
 	inc l
 	ld (hl),d
@@ -433,7 +428,7 @@ ENDM
 	pop de
 	ld (hl),e
 	inc l
-	ld (hl),d	
+	ld (hl),d
 	jp _print_char_rest_sp
 	; ---------------------------
 	; фон есть, если ли изображение одновременно с фоном?
@@ -442,7 +437,7 @@ _print_char_is_color:
 	and $0f						; выделяем только цвет символа
 	and c						; наложить бит цвета для этой плоскости
 	jp z,_print_char_inv_char	; если нет цвета, только фон, переход
-	; ---------------------------	
+	; ---------------------------
 	; есть и цвет и фон
 	xor a						; заполняем значением $FF (8 байт)
 	cpl
@@ -452,7 +447,7 @@ REPT 7
 ENDM
 	ld (hl),a
 	jp _print_char_restore_l
-	; ---------------------------	
+	; ---------------------------
 	; выводить инверсный вариант символа (8 байт)
 _print_char_inv_char:
 REPT 3
@@ -464,7 +459,7 @@ REPT 3
 	ld a,d
 	cpl
 	ld (hl),a
-	inc l	
+	inc l
 ENDM
 	pop de
 	ld a,e
@@ -474,27 +469,27 @@ ENDM
 	ld a,d
 	cpl
 	ld (hl),a
-	; ---------------------------	
+	; ---------------------------
 	; восстановить SP на начало символа
-_print_char_rest_sp:			
+_print_char_rest_sp:
 	ex de,hl
 	ld hl,(cur_char_addr)
 	ld sp,hl
 	ex de,hl
 	; ---------------------------
 	; восстановить L и сменить плоскость в H
-_print_char_restore_l:		
-	ld a,l					
+_print_char_restore_l:
+	ld a,l
 	sub 7
 	ld l,a
-_print_char_next_plane:	
-	ld a,h					
+_print_char_next_plane:
+	ld a,h
 	sub $20
 	ld h,a
 	; ---------------------------
 	; сдвинуть маску цвет/фон
 _print_char_shift_mask:
-	ld a,c					
+	ld a,c
 	rlca
 	ld c,a
 	jp nc,_print_char_repeat	; повторить, если еще есть плоскости
@@ -526,7 +521,7 @@ palette_off:
 
 	; ******************************
 	; Процедура set_palette - программирование палитры.
-    ; Адрес палитры  
+    ; Адрес палитры
 	; ******************************
 setup_palette:
 	push af
@@ -544,29 +539,53 @@ _setup_palette_repeat:
 	out ($02),a
 	ld a,(hl)			; по адресу в ячейке bc взять значение цвета
 	out ($0c),a
-	inc hl				; след. ячейка палитры 
+	inc hl				; след. ячейка палитры
 	out ($0c),a
-	inc c			; след. номер цвета	
+	inc c			; след. номер цвета
 	out ($0c),a
 	dec b			; уменьшаем счетчик
 	out ($0c),a
 	jp p,_setup_palette_repeat
 	ld a,8			; цвет бордюра такой же как фона
-	out ($02),a		
+	out ($02),a
 	pop bc
 	pop hl
 	pop af
 	ret
 
+cur_char_pos:
+	DB 0,0			; хранится текущее положение курсора y,x (0-31)
+
+tab_width:
+	DB 4			; длина табуляции (0-31)
+
+tab_placeholder:
+	DEFM " "		; по умолчанию пробел
+
+char_color:			; текущий цвет символов и фона (символ + 16 * фон)
+	DB %01010011
+
+saved_sp:			; временное хранение значения регистра SP
+	DW $0000
+
+cur_char_addr:		; сохраняем адрес текущего символа в таблице шрифта
+	DW $0000
+
+scr_addr_char:		; адрес на экране для вывода символа
+	DW $0000
+
+scr_mask:			; маска запрета экранных плоскостей
+	DW %00001111
+
     ; адрес текущей палитры
 palette_cur:
-    dw palette_0
+    DW palette_0
 
 	;  палитра рабочая
 palette_1:
 	;   с к ч  з яз кп  ж  б
-	db 64,128,16,208,6,134,22,54,0,197,34,192,2,152,82,173
+	DB 64,128,16,208,6,134,22,54,0,197,34,192,2,152,82,173
 
 	;  палитра для подготовки экрана
 palette_0:
-	db 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+	DB 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
--- utils_demo.asm
+++ utils_demo.asm
@@ -2,28 +2,28 @@
 
 	jp start_prog
 
-	include "koi8r-font.asm"	
+	include "koi8r-font.asm"
 	include "utils.asm"
 	include "rst7.asm"
 	include "strings.asm"
 
 start_prog:
 	di					; запретить прерывания
-	xor a				; выключить квазидиск 
-	out ($10),a	
+	xor a				; выключить квазидиск
+	out ($10),a
 	ld sp,$8000			; инициализировать указатель стека
 
 	ld a,$C3			; код инструкции jp start_prog
 	ld ($0000),a		; записать по адресу 0
-	ld hl,start_prog	
-	ld ($0001),hl	
+	ld hl,start_prog
+	ld ($0001),hl
 
 	ld ($0038),a		; записать команду jp int_proc
 	ld hl,int_proc		; по адресу 0x38 (адрес прерывания)
 	ld ($0039),hl
 
 	ei					; разрешить прерывания
-	
+
 	call palette_off	; выключить палитру
 	call clear_scr		; очистить экран
 	call palette_on		; включить палитру
@@ -31,7 +31,7 @@ start_prog:
 	ld a,%00001111		; разрешить доступ ко всем плоскостям (0-4)
 	call set_planes
 
-	ld a,14				; начальный цвет символов
+	ld a,15				; начальный цвет символов
 	call set_color		; установить
 
 	ld hl,$001f			; начальная координата h=x, l=y
@@ -47,7 +47,7 @@ start_prog:
 	call set_tab_placeholder
 
 	ld de,str_test2		; строка для вывода
-	call print_str		; печатать	
+	call print_str		; печатать
 
 loop:
 	nop					; бесконечный цикл
